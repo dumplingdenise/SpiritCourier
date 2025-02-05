41bcb4e2698837f4361e7a22d8c3ba4a2d3dd224
Denise <deniseteo02@gmail.com>
2025-02-05 20:04:23 +0800

    spawn parcels

--- Assets/Scripts/SpawnParcel.cs
+++ Assets/Scripts/SpawnParcel.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class SpawnParcel : MonoBehaviour
 {
     public GameObject[] Parcel;
@@ -12,15 +13,27 @@ public class SpawnParcel : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
     private List<ParcelData> spawnedParcels = new List<ParcelData>();
+
+    [System.Serializable] // Make this class show up in the inspector
+    public class OneRectangle 
+    { // this class ask for 2 Vector2 
+        public Vector2 rectBottomLeft;
+        public Vector2 rectTopRight;
+    }
+    [SerializeField]
+    public List<OneRectangle> noParcelsAreas = new List<OneRectangle>(); // Definiing a list of oneRectangle
+    // Every rectangle helps to prevent parcel from spawning inside that area
     
     void Start()
     {
         StartCoroutine(SpawnParcels());
     }
 
-    IEnumerator SpawnParcels() {
+    IEnumerator SpawnParcels() 
+    {
         int spawnCount = 0;
-        while (spawnCount < 10) {
+        while (spawnCount < 10) 
+        {
             GameObject selectedParcel = Parcel[Random.Range(0, Parcel.Length)];
             
             Vector2 spawnPos = new Vector2(
@@ -28,7 +41,8 @@ public class SpawnParcel : MonoBehaviour
                 Random.Range(-areaSize.y / 2, areaSize.y / 2)
             ) + (Vector2)transform.position;
 
-            if (!IsPositionTooClose(spawnPos, selectedParcel)) {
+            if (!IsPositionTooClose(spawnPos, selectedParcel) && !IsSpawningInExcludedArea(spawnPos)) 
+            {
                 Instantiate(selectedParcel, spawnPos, Quaternion.identity);
                 spawnedParcels.Add(new ParcelData(spawnPos, selectedParcel));
                 spawnCount++;
@@ -38,14 +52,33 @@ public class SpawnParcel : MonoBehaviour
         }
     }
 
-    bool IsPositionTooClose(Vector2 position, GameObject Parcel) {
-        float parcelRadius = GetParcelSize(Parcel);
-        foreach (ParcelData spawnedParcel in spawnedParcels) {
-            if (Vector2.Distance(spawnedParcel.position, position) < parcelRadius + minDistBetweenParcels) {
+    bool IsPositionTooClose(Vector2 posOfSpawnedParcel, GameObject ParcelObj) 
+    {
+        float parcelRadius = GetParcelSize(ParcelObj);
+        foreach (ParcelData spawnedParcel in spawnedParcels) 
+        {
+            if (Vector2.Distance(spawnedParcel.position, posOfSpawnedParcel) < parcelRadius + minDistBetweenParcels) 
+            {
                 return true;
             }
         }
 
+        return false;
+    }
+
+    bool IsSpawningInExcludedArea(Vector2 posOfSpawnParcel)
+    {
+        foreach (OneRectangle rectangle in noParcelsAreas) 
+        {
+            if (rectangle.rectBottomLeft.x < posOfSpawnParcel.x && posOfSpawnParcel.x < rectangle.rectTopRight.x)
+            {
+                if (rectangle.rectBottomLeft.y < posOfSpawnParcel.y && posOfSpawnParcel.y < rectangle.rectTopRight.y)
+                {
+                    Debug.Log("Illegally spawn");
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -57,19 +90,49 @@ public class SpawnParcel : MonoBehaviour
         return Mathf.Max(parcelWidth, parcelHeight) / 2f;
     }
 
-    class ParcelData {
+    class ParcelData
+    {
         public Vector2 position;
         public GameObject Parcel;
 
-        public ParcelData(Vector2 position, GameObject Parcel) { 
+        public ParcelData(Vector2 position, GameObject Parcel)
+        { 
             this.position = position;
             this.Parcel = Parcel;
         }
     }
 
-    // Update is called once per frame
-  /*  void Update()
+    private void OnDrawGizmosSelected()
     {
-        
-    }*/
+        //Gizmos.DrawWireCube draw the outline DrawCube draws a translucent cube
+
+        foreach (OneRectangle rectangle in noParcelsAreas)
+        {
+            if (rectangle.rectBottomLeft.x < rectangle.rectTopRight.x && rectangle.rectBottomLeft.y < rectangle.rectTopRight.y)
+            {
+                Gizmos.color = Color.green;
+            }
+            else
+            {
+                Gizmos.color = Color.red;
+            }
+
+            Vector3 center = Vector3.Lerp(rectangle.rectTopRight, rectangle.rectBottomLeft, 0.5f);
+            float length = rectangle.rectTopRight.x - rectangle.rectBottomLeft.x;
+            float breadth = rectangle.rectTopRight.y - rectangle.rectBottomLeft.y;
+
+            Gizmos.DrawWireCube(center, new Vector3(length, breadth, 1f));
+
+        }
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, areaSize);
+
+    }
+
+    // Update is called once per frame
+    /*  void Update()
+      {
+
+      }*/
 }
