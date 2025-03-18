3f4862286bc2c723bcf6681677c1f3dbb530d944
Denise <deniseteo02@gmail.com>
2025-03-19 02:04:03 +0800

    idk

--- Assets/Scripts/Puzzles/Cross.cs
+++ Assets/Scripts/Puzzles/Cross.cs
@@ -52,11 +52,19 @@ public class Cross : MonoBehaviour
         }
 
         GameController gameController = FindAnyObjectByType<GameController>();
-        DialogManager dialogMang = FindAnyObjectByType<DialogManager>();
+        DialogManager dialogManager = FindAnyObjectByType<DialogManager>();
+        Inventory inventoryManager = FindAnyObjectByType<Inventory>();
         if (gameController != null)
         {
-            gameController.SetGameState(GameState.WaitingForDelivery); // Resume gameplay
-            dialogMang.CloseDialog();
+            // test 
+            if (inventoryManager.GetInventoryList() != null)
+            {
+                gameController.SetGameState(GameState.WaitingForDelivery); // Resume gameplay
+            }
+            gameController.SetGameState(GameState.FreeRoam);
+
+            /*gameController.SetGameState(GameState.WaitingForDelivery); // Resume gameplay*/
+            dialogManager.CloseDialog();
         }
 
 
