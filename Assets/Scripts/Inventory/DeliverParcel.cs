c6c5dbf63ebaa0f3ee432ae7721a7ba1dbd0cfa2
shumineo <teoshumin123@gmail.com>
2025-03-19 17:22:22 +0800

    ...

--- Assets/Scripts/Inventory/DeliverParcel.cs
+++ Assets/Scripts/Inventory/DeliverParcel.cs
@@ -371,7 +371,7 @@ public class DeliverParcel : MonoBehaviour
         var uiDocument = GetComponentInParent<UIDocument>();
         if (uiDocument != null)
         {
-            var rootVisualElement = uiDocument.rootVisualElement; // Get the UIDocument in the spawned parcel gameObject.
+            var rootVisualElement = uiDocument.rootVisualElement; // Get the UIDocument in the spawned parcel gameobject.
 
             /*promptText = rootVisualElement.Q("DeliverLabel"); // get the Label*/
 
@@ -381,15 +381,6 @@ public class DeliverParcel : MonoBehaviour
             {
                 promptText.style.display = DisplayStyle.None; // do not display it on start
             }
-            else
-            {
-                
-                Debug.LogError("Cant find label");
-            }
-        }
-        else
-        {
-            Debug.LogError("Cant find UI DOC");
         }
     }
 
@@ -527,14 +518,12 @@ public class DeliverParcel : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.LogError("Pressing E");
             if (!playerNearby)
             {
                 return; // Exit to prevent parcel removal
             }
             else
             {
-                Debug.LogError("Player near NPC");
                 if (gameController.GetCurrentState() == GameState.FreeRoam)
                 {
                     if (inventoryList.Count == 0 || inventory.selectedSlot < 0)
--- Assets/Scripts/Inventory/Parcels.cs
+++ Assets/Scripts/Inventory/Parcels.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
@@ -12,7 +12,7 @@ public class Parcels : MonoBehaviour
     [System.Serializable]
     public class ParcelData
     {
-        *//*public GameObject parcelObject;*//*
+        /*public GameObject parcelObject;*/
         public Vector2 position;
         public int parcelID;
         public string parcelName;
@@ -23,9 +23,9 @@ public class Parcels : MonoBehaviour
         // test code
         public Dialog parcelStoryDialog;
 
-        public ParcelData(*//*GameObject parcelObject,*//* Vector2 position, int parcelID, string parcelName, Sprite parcelSprite, MainNpcs.NPCData assignedNpcData, string parcelHints, string npcHints, Dialog parcelStoryDialog) 
+        public ParcelData(/*GameObject parcelObject,*/ Vector2 position, int parcelID, string parcelName, Sprite parcelSprite, MainNpcs.NPCData assignedNpcData, string parcelHints, string npcHints, Dialog parcelStoryDialog)
         {
-            *//*this.parcelObject = parcelObject;*//*
+            /*this.parcelObject = parcelObject;*/
             this.position = position;
             this.parcelID = parcelID;
             this.parcelName = parcelName;
@@ -43,11 +43,11 @@ public class Parcels : MonoBehaviour
         npcList = FindAnyObjectByType<MainNpcs>().GetNPCList();
         AssignParcels();
 
-       *//* // Make the parcel object hidden on start
-        foreach (GameObject parcel in ParcelsObjects)
-        {
-            parcel.SetActive(false);
-        }*//*
+        /* // Make the parcel object hidden on start
+         foreach (GameObject parcel in ParcelsObjects)
+         {
+             parcel.SetActive(false);
+         }*/
     }
 
     void AssignParcels()
@@ -57,7 +57,7 @@ public class Parcels : MonoBehaviour
 
         foreach (GameObject parcelObj in ParcelsObjects)
         {
-            *//*GameObject parcelObject = parcelObj;*//*
+            /*GameObject parcelObject = parcelObj;*/
             Sprite parcelSprite = parcelObj.GetComponent<SpriteRenderer>().sprite;
             string parcelName = parcelObj.name;
 
@@ -66,14 +66,14 @@ public class Parcels : MonoBehaviour
             string parcelHints = pickUpParcel.parcelHints;
             string npcHints = pickUpParcel.npcHints;
             Dialog parcelStorydialog = pickUpParcel.parcelStoryDialog; // test code
-            
+
             if (pickUpParcel != null)
             {
                 // Ensure that you have an NPC to assign to the parcel
                 if (pickUpParcel.assignedNPC != null)
                 {
                     ParcelData parcelData = new ParcelData(
-                        *//*parcelObject,*//*
+                        /*parcelObject,*/
                         (Vector2)parcelObj.transform.position,
                         parcelID,
                         parcelName,
@@ -88,7 +88,7 @@ public class Parcels : MonoBehaviour
 
                     // Pass the assigned NPC data to the PickUpParcel component
                     pickUpParcel.parcelData = parcelData;
-                    
+
                     Quest.questData newQuest = new Quest.questData(parcelID, Quest.questType.findParcel, Quest.questStatus.inActive, parcelData, parcelObj);
                     questManager.addParcelDatatoQuest(newQuest);
 
@@ -114,292 +114,6 @@ public class Parcels : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-}
-*/
 
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using UnityEngine;
-using static Quest;
-
-public class Parcels : MonoBehaviour
-{
-    public GameObject[] ParcelsObjects;
-    private List<ParcelData> assignedParcels = new List<ParcelData>();
-    private List<MainNpcs.NPCData> npcList; // declares a private list to store NPC data
-    private List<int> pickedUpParcelIDs = new List<int>();
-
-    // test code
-    public static Parcels Instance { get; private set; }
-
-
-    [System.Serializable]
-    public class ParcelData
-    {
-        /*public GameObject parcelObject;*/
-        public Vector2 position;
-        public int parcelID;
-        public string parcelName;
-        public Sprite parcelSprite;
-        public MainNpcs.NPCData assignedNpcData;
-        public string parcelHints;
-        public string npcHints;
-        public Dialog parcelStoryDialog;
-        // test
-        public bool isActiveQuest;
-
-        public ParcelData(Vector2 position, int parcelID, string parcelName, Sprite parcelSprite,
-            MainNpcs.NPCData assignedNpcData, string parcelHints, string npcHints, Dialog parcelStoryDialog, bool isActiveQuest)
-        {
-            this.position = position;
-            this.parcelID = parcelID;
-            this.parcelName = parcelName;
-            this.parcelSprite = parcelSprite;
-            this.assignedNpcData = assignedNpcData;
-            this.parcelHints = parcelHints;
-            this.npcHints = npcHints;
-            this.parcelStoryDialog = parcelStoryDialog;
-            this.isActiveQuest = isActiveQuest;
-        }
-    }
-
-    void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);  // Keeps the parent object with all parcels
-        }
-        else
-        {
-            Destroy(gameObject);  // Prevent duplicate instances
-        }
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        npcList = FindAnyObjectByType<MainNpcs>().GetNPCList();
-        AssignParcels();
-        UpdateParcelVisibility();
-
-        // test code
-        /*npcList = FindAnyObjectByType<MainNpcs>().GetNPCList();
-        LoadPickedUpParcels(); // Load picked-up parcels when scene loads
-        AssignParcels();
-        Quest.Instance.FillActiveQuests(); // Force refresh active quests
-        UpdateParcelVisibility();*/
-
-    }
-
-    void AssignParcels()
-    {
-        int parcelID = 0;
-        Quest questManager = FindAnyObjectByType<Quest>();
-
-        foreach (GameObject parcelObj in ParcelsObjects)
-        {
-            Sprite parcelSprite = parcelObj.GetComponent<SpriteRenderer>().sprite;
-            string parcelName = parcelObj.name;
-
-            PickUpParcel pickUpParcel = parcelObj.GetComponent<PickUpParcel>();
-
-            string parcelHints = pickUpParcel.parcelHints;
-            string npcHints = pickUpParcel.npcHints;
-            Dialog parcelStorydialog = pickUpParcel.parcelStoryDialog;
-
-            if (pickUpParcel != null)
-            {
-                // Ensure that you have an NPC to assign to the parcel
-                if (pickUpParcel.assignedNPC != null)
-                {
-                    // test
-                    // Check if this parcel is part of an active quest
-                    bool isActiveQuest = questManager.IsQuestActive(parcelID); // Check if this parcel is assigned to an active quest
-                    Debug.LogError($"ParcelID: {parcelID}, IsActiveQuest: {isActiveQuest}");
-
-                    ParcelData parcelData = new ParcelData(
-                        (Vector2)parcelObj.transform.position,
-                        parcelID,
-                        parcelName,
-                        parcelSprite,
-                        pickUpParcel.assignedNPC,
-                        parcelHints,
-                        npcHints,
-                        parcelStorydialog,
-                        // test
-                        isActiveQuest
-                        );
-                    assignedParcels.Add(parcelData);
-
-
-                    // Pass the assigned NPC data to the PickUpParcel component
-                    pickUpParcel.parcelData = parcelData;
-
-                    // Check if parcelID already exists in the quest list
-                    if (!questManager.IsParcelInQuest(parcelID))
-                    {
-                        Quest.questData newQuest = new Quest.questData(parcelID, Quest.questType.findParcel, Quest.questStatus.inActive, parcelData, parcelObj, isActiveQuest);
-                        questManager.addParcelDatatoQuest(newQuest);
-                        Debug.LogError($"Parcel ID {parcelID} adding to quest");
-                    }
-                    else
-                    {
-                        Debug.LogError($"Parcel ID {parcelID} already exists in the quest list. Skipping...");
-                    }
-
-                    Debug.Log($"Parcel Name: {parcelName}, Parcel ID: {parcelID}, Sprite: {parcelSprite} at {parcelObj.transform.position} assigned to NPC ID: {pickUpParcel.assignedNPC.npcID}");
-                }
-                else
-                {
-                    Debug.LogError($"No NPC assigned to parcel at {parcelObj.transform.position}");
-                }
-
-                parcelID++;
-            }
-            else
-            {
-                Debug.LogError($"No PickUpParcel component found on parcel at {parcelObj.transform.position}");
-            }
-        }
-        questManager.FillActiveQuests();
-    }
-
-    // test code
-    public void MarkParcelPicked(int parcelID)
-    {
-        if (!pickedUpParcelIDs.Contains(parcelID))
-        {
-            pickedUpParcelIDs.Add(parcelID);
-            Debug.Log($"Parcel {parcelID} picked up.");
-        }
-    }
-
-
-    /*    public void MarkParcelPicked(int parcelID)
-        {
-            if (!pickedUpParcelIDs.Contains(parcelID))
-            {
-                pickedUpParcelIDs.Add(parcelID);
-                SavePickedUpParcels();  // Save to persistence
-                Debug.Log($"Parcel {parcelID} picked up.");
-            }
-        }
-
-        private void SavePickedUpParcels()
-        {
-            PlayerPrefs.SetString("PickedUpParcels", string.Join(",", pickedUpParcelIDs));
-            PlayerPrefs.Save();
-        }
-
-        private void LoadPickedUpParcels()
-        {
-            if (PlayerPrefs.HasKey("PickedUpParcels"))
-            {
-                string savedIDs = PlayerPrefs.GetString("PickedUpParcels");
-                pickedUpParcelIDs = savedIDs.Split(',').Where(s => int.TryParse(s, out _)).Select(int.Parse).ToList();
-            }
-        }*/
-
-    /*public void UpdateParcelVisibility()
-    {
-        Inventory inventoryManager = FindAnyObjectByType<Inventory>();
-        List<int> pickedUpParcelIDs = new List<int>();
-
-        foreach (var parcelData in inventoryManager.GetInventoryList())
-        {
-            pickedUpParcelIDs.Add(parcelData.parcelID);
-        }
-
-
-        foreach (var parcel in assignedParcels)
-        {
-            if (parcel.parcelID < ParcelsObjects.Length)
-            {
-                GameObject parcelObj = ParcelsObjects[parcel.parcelID];
-                Debug.LogError($"Parcel {parcel.parcelID} visibility set to: {parcel.isActiveQuest}");
-                // If parcel has been picked up already, don't show it again
-                if (pickedUpParcelIDs.Contains(parcel.parcelID))
-                {
-                    parcelObj.SetActive(false); // Hide it if already in the inventory
-                }
-                else
-                {
-                    if (parcel.isActiveQuest)
-                    {
-                        parcelObj.SetActive(true); // Show if quest is active and not already picked up
-                    }
-                    else
-                    {
-                        parcelObj.SetActive(false); // Hide if quest is not active
-                    }
-                }
-            }
-            else
-            {
-                Debug.LogError($"Parcel ID {parcel.parcelID} is out of bounds for ParcelsObjects array.");
-            }
-        }
-    }*/
-    public void UpdateParcelVisibility()
-    {
-        foreach (var parcel in assignedParcels)
-        {
-            if (parcel.parcelID < ParcelsObjects.Length)
-            {
-                GameObject parcelObj = ParcelsObjects[parcel.parcelID];
-                Debug.LogError($"Parcel {parcel.parcelID} visibility set to: {parcel.isActiveQuest}");
-                // If parcel has been picked up already, don't show it again
-                if (pickedUpParcelIDs.Contains(parcel.parcelID))
-                {
-                    parcelObj.SetActive(false); // Hide it if already in the inventory
-                }
-                else
-                {
-                    if (parcel.isActiveQuest)
-                    {
-                        parcelObj.SetActive(true); // Show if quest is active and not already picked up
-                    }
-                    else
-                    {
-                        parcelObj.SetActive(false); // Hide if quest is not active
-                    }
-                }
-            }
-            else
-            {
-                Debug.LogError($"Parcel ID {parcel.parcelID} is out of bounds for ParcelsObjects array.");
-            }
-        }
-    }
-
-    /*public void UpdateParcelVisibility()
-    {
-        foreach (var parcel in assignedParcels)
-        {
-            if (parcel.parcelID < ParcelsObjects.Length)
-            {
-                GameObject parcelObj = ParcelsObjects[parcel.parcelID];
-                if (pickedUpParcelIDs.Contains(parcel.parcelID))
-                {
-                    parcelObj.SetActive(false);
-                }
-                else
-                {
-                    parcelObj.SetActive(parcel.isActiveQuest);
-                }
-            }
-        }
-    }*/
-
-    // Update is called once per frame
-    void Update()
-    {
-        // test code
-        /*UpdateParcelVisibility();*/
     }
 }
